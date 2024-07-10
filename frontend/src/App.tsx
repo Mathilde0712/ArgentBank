0f1a7ae1e65f9d2c4f1c19a1610b1849b0aabb4d
Mathilde Besse <mathildebesse07@gmail.com>
2024-07-10 16:18:08 +0200

    modification des noms des dossiers pour soutenance

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,33 +1,32 @@
-
-import  { BrowserRouter, Routes, Route } from "react-router-dom";
-import "../src/designs/css/main.css"
-import Home from './Pages/Home';
-import SignIn from "./Pages/SignIn";
-import User from "./Pages/User";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "../src/designs/css/main.css";
+import Home from "./pages/Home";
+import SignIn from "./pages/SignIn";
+import User from "./pages/User";
 import { useSelector } from "react-redux";
 import { RootState } from "./app/store";
-import UserProfile from "./Pages/UserProfile";
+import UserProfile from "./pages/UserProfile";
 
 function App() {
   const token = useSelector((state: RootState) => state.auth.token);
 
   return (
     <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<Home/>}/>
-      <Route path="/sign-in" element={<SignIn />}/>
-      {token ? (
-       <Route path="/user" element={<User />}/>
-      ) : (
-        <Route path="/user" element={<SignIn />}/>
-      )}
-            {token ? (
-       <Route path="/profile" element={<UserProfile />}/>
-      ) : (
-        <Route path="/profile" element={<SignIn />}/>
-      )}
-    {/*<Route path="*" element={}/> */}
-    </Routes>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        {token ? (
+          <Route path="/user" element={<User />} />
+        ) : (
+          <Route path="/user" element={<SignIn />} />
+        )}
+        {token ? (
+          <Route path="/profile" element={<UserProfile />} />
+        ) : (
+          <Route path="/profile" element={<SignIn />} />
+        )}
+        {/*<Route path="*" element={}/> */}
+      </Routes>
     </BrowserRouter>
   );
 }
